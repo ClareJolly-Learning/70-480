7bc07a31c50947b6a773a09ac5d8f79c19511bdb
Clare Jolly <clare.jolly@vodafone.com>
2019-07-15 16:09:45 +0100

    arrays

--- jumpstart/demo/4-demo.js
+++ jumpstart/demo/4-demo.js
@@ -1,43 +1,12 @@
-var a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var s = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india"];
+var fruit = ['orange', 'pear', 'pineapple', 'apple', 'cherry'];
 
-//map
-var aPlus1 = a.map(function (i) { return i + 1; });
-console.log(aPlus1)
+fruit.forEach(function(item){
+  console.log(item)
+})
+  console.log(fruit.some(function(item, index) {
+    return item[0] === 'a';
+  })) // true
 
-
-//filter
-var bigNumbers = a.filter(function (i) { return i > 5; });
-console.log(bigNumbers)
-
-
-//some
-var hasCharlie = s.some(function (i) { return i == "charlie"; });
-console.log(hasCharlie)
-
-
-//every
-var allPositive = a.every(function (i) { return i >= 0; });
-console.log(allPositive)
-
-
-//forEach
-s.forEach(function(item, index) {
-console.log(item)
-});
-
-//reduce
-var sum = a.reduce(function (previous, current) { return previous + current; });
-console.log(sum)
-
-//sort
-var aReversed = a.reverse();
-console.log(aReversed);
-
-var sorted = aReversed.sort();
-console.log(sorted);
-
-//splice
-var aSpliced = a;
-aSpliced.splice(2, 2, "20", "21");
-console.log(aSpliced)
+  console.log(fruit.some(function(item, index) {
+    return item.length < 1;
+  })) // false
