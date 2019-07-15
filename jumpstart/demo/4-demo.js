a31e621913dca94cabb28ce950fb3f294d8e7150
Clare Jolly <clare.jolly@vodafone.com>
2019-07-15 15:14:57 +0100

    array functions

--- jumpstart/demo/4-demo.js
+++ jumpstart/demo/4-demo.js
@@ -1,13 +1,43 @@
-function add(n1, n2) {
-  return n1 + n2
-}
+var a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var s = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india"];
 
-function calc(n1, n2, processForCalc) {
-  return processForCalc(n1, n2);
-}
+//map
+var aPlus1 = a.map(function (i) { return i + 1; });
+console.log(aPlus1)
 
-function executeMath() {
-  console.log(calc(4,4,add));
-}
 
-executeMath()
+//filter
+var bigNumbers = a.filter(function (i) { return i > 5; });
+console.log(bigNumbers)
+
+
+//some
+var hasCharlie = s.some(function (i) { return i == "charlie"; });
+console.log(hasCharlie)
+
+
+//every
+var allPositive = a.every(function (i) { return i >= 0; });
+console.log(allPositive)
+
+
+//forEach
+s.forEach(function(item, index) {
+console.log(item)
+});
+
+//reduce
+var sum = a.reduce(function (previous, current) { return previous + current; });
+console.log(sum)
+
+//sort
+var aReversed = a.reverse();
+console.log(aReversed);
+
+var sorted = aReversed.sort();
+console.log(sorted);
+
+//splice
+var aSpliced = a;
+aSpliced.splice(2, 2, "20", "21");
+console.log(aSpliced)
